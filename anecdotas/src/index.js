3c44a821866af6d60d7fd7b8f28faf0262429c0a
unknown <franco.starke@tecsup.edu.pe>
2021-04-14 16:23:52 -0500

    Anecdota del dia y anecdota con mas votos

--- anecdotas/src/index.js
+++ anecdotas/src/index.js
@@ -4,12 +4,36 @@ import ReactDOM from 'react-dom'
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votaciones,setVotaciones] = useState([0])
-
+  const [aceptacion, setAceptacion] = useState(-1)
   const [contador, setContador] = useState(0)
 
+  
   const Siguiente = () => {
       setSelected(getRandomInt(0,anecdotes.length))
+      
   }
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+    calcularAceptacion()
+  }
+
+  function calcularAceptacion(){
+    console.log(votaciones)
+    let index = 0
+    let mayor = 0
+    while (index < anecdotes.length) {
+      if (votaciones[index]) {
+        
+        if (votaciones[index]>mayor) {
+          mayor = votaciones[index]
+          setAceptacion(index)
+        }
+      }
+      index++
+    }
+  }
+  
 
   const Voto = () => {
       if(!votaciones[selected]){
@@ -18,20 +42,38 @@ const App = (props) => {
       votaciones[selected]=votaciones[selected]+1
       setVotaciones(votaciones)
       setContador(contador+1)
+
+      calcularAceptacion()
+      
   }
 
-  function getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-  }
+  
+
+  
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {props.anecdotes[selected]}
+      
           <br/>
           has {votaciones[selected]} votes
           <hr/>
+          <br/>
           <button onClick={Voto}>Vote</button>
           <button onClick={Siguiente}>Next Anecdote</button>
+          <br/>
+          {
+              (aceptacion>=0) ?
+                  <div>
+                      <h1>Anecdote with most votes</h1>
+                      {props.anecdotes[aceptacion]}
+                      <br/>
+                      has {votaciones[aceptacion]} votes
+                  </div>
+                  :
+                  <div></div>
+          }
     </div>
   )
 }
